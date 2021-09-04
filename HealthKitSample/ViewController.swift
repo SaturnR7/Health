1bad0373b45befe1528ed94ac1e3052d759286b5
SaturnR7 <hidemasa.kobayashi@gmail.com>
2021-09-04 11:47:55 +0900

    Initial Commit

---
+++ HealthKitSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthKitSample
+//
+//  Created by Hidemasa Kobayashi on 2021/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
