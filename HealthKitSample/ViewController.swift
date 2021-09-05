2a7a22974c1003ffc113733cb42dc829729fceac
SaturnR7 <hidemasa.kobayashi@gmail.com>
2021-09-05 13:49:26 +0900

    コメントを追加

--- HealthKitSample/ViewController.swift
+++ HealthKitSample/ViewController.swift
@@ -14,11 +14,11 @@ final class ViewController: UIViewController {
         
         super.viewDidLoad()
         
-        /// アクセスしたいデータ項目のタイプを `Set型` で格納する。
-        /// 今回は歩数を取得したいので `.stepCount` を指定する。
-        /// また、兵器歩行速度なら `.walkingSpeed`、歩行距離なら `.distanceWalkingRunning`など、許可を得たい項目を配列で指定する。
+        /// ユーザーにヘルスケアへのアクセス許可を求める。
+        ///
+        /// アクセスしたいサンプルデータのタイプを `Set型` で格納する。今回は歩数を取得したいので `.stepCount` を指定する。
+        /// また、平均歩行速度なら `.walkingSpeed`、歩行距離なら `.distanceWalkingRunning`など、許可を得たい項目を配列で指定する。
         let readDataTypes = Set([HKObjectType.quantityType(forIdentifier: .stepCount)!])
-        /// ユーザーにアクセス許可を求める。
         HKHealthStore().requestAuthorization(toShare: nil, read: readDataTypes) { _, _ in }
         
         self.getSteps()
@@ -28,23 +28,21 @@ final class ViewController: UIViewController {
         
         var sampleArray: [Double] = []
         
-        /// ７日前の日付を取得し、`startOfDay`で日の始まりを取得する。
+        /// 取得したいサンプルデータの期間の開始日を指定する。（今回は７日前の日付を取得する。）
         let sevenDaysAgo = Calendar.current.date(byAdding: DateComponents(day: -7), to: Date())!
         let startDate = Calendar.current.startOfDay(for: sevenDaysAgo)
 
-        /// サンプルデータの検索条件を指定する。（サンプルデータのフィルタリング）
+        /// サンプルデータの検索条件を指定する。（フィルタリング）
         /// 条件は、取得したいサンプルデータの期間（開始日〜終了日）を指定する。
         ///
         /// - Parameters:
         ///   - withStart: サンプルデータの取得開始日を指定する。
         ///   - end: サンプルデータの終了日を指定する。
-        ///   - options:取得範囲の指定（？）→ オプションの機能が分かっていません。
-        ///             （strictStartDate指定で開始日から狙ったサンプルデータは取得できています）
-        ///             （こちらも詳しい情報ありましたらコメントいただけると幸いです）
+        ///   - options: 今回は不使用（詳しい情報ご存知でしたらコメントいただけると幸いです）
         ///   @see https://developer.apple.com/documentation/healthkit/hkquery/1614771-predicateforsamples/
         let predicate = HKQuery.predicateForSamples(withStart: startDate,
                                                     end: Date(),
-                                                    options: .strictStartDate)
+                                                    options: [])
         
         /// サンプルデータを取得するためのクエリを生成する。
         ///
@@ -54,7 +52,7 @@ final class ViewController: UIViewController {
         ///   - options: サンプルデータの計算方法を指定する。今回は１日の合計歩数がほしいので `cumulativeSum` を指定する。
         ///   - anchorDate: 基準となる日付（時間を数直線とした場合に、アンカー日付は原点で過去・未来両方に伸びる目盛りが作成される。（Documentより））
         ///                 今回の場合、開始日にアンカーを指定してあげれば、指定した機関のサンプルが取得できます。
-        ///                 （こちらも詳しい情報ありましたらコメントいただけると幸いです）
+        ///                 （詳しい情報ご存知でしたらコメントいただけると幸いです）
         ///   - intervalComponents: サンプルの時間間隔の長さを指定する。今回は日別に歩数を取得したいので、間隔は１日で指定する。
         ///   @see https://developer.apple.com/documentation/healthkit/hkstatisticscollectionquery/1615199-init
         let query = HKStatisticsCollectionQuery(quantityType: HKObjectType.quantityType(forIdentifier: .stepCount)!,
@@ -70,10 +68,10 @@ final class ViewController: UIViewController {
             guard let statsCollection = results else { return }
             /// クエリ結果から期間（開始日・終了日）を指定して歩数の統計情報を取り出す。
             statsCollection.enumerateStatistics(from: startDate, to: Date()) { statistics, _ in
-                /// `statistics` から最小単位（今回は１日分の歩数）のサンプルデータが返ってくる。
-                /// `sumQuantity` でサンプルデータの合計（１日の合計歩数）を取得する。
+                /// `statistics` に最小単位（今回は１日分の歩数）のサンプルデータが返ってくる。
+                /// `statistics.sumQuantity()` でサンプルデータの合計（１日の合計歩数）を取得する。
                 if let quantity = statistics.sumQuantity() {
-                    /// サンプルデータは`doubleValue (戻り値はDouble型)`で取り出し、単位を指定して取得する。
+                    /// サンプルデータは`quantity.doubleValue`で取り出し、単位を指定して取得する。
                     /// 単位：歩数の場合`HKUnit.count()`と指定する。（歩行速度の場合：`HKUnit.meter()`、歩行距離の場合：`HKUnit(from: "m/s")`といった単位を指定する。）
                     let stepValue = quantity.doubleValue(for: HKUnit.count())
                     /// 取得した歩数を配列に格納する。
